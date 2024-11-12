bfcfcf39b3d2f0fd32082d552e1b6ecc014e6e18
Tyson <tyson@chainsecurity.asia>
2024-11-12 16:53:43 +0800

    first commit: fix env & webpack

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,9 @@
 'use strict';
 const mongoose = require('mongoose');
-const dbConnection = process.env.DB_CONNECTION;
+const dbConnection = process.env.DB_CONNECTION || "mongodb://127.0.0.1:27017/webauthn";
+
+console.log("dbConnection :", dbConnection);
+
 mongoose.set('useCreateIndex', true);
 
 mongoose.connect(dbConnection, {
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,64 @@
-'use strict';
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+"use strict";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const serverPort = process.env.PORT || 8080;
+
 module.exports = {
-	entry: ['babel-polyfill', './client/index.js'],
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-				loader: 'url-loader?limit=100000',
-			},
-		],
-	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx'],
-	},
-	devServer: {
-		port: 3000,
-		open: true,
-		proxy: {
-			'/webauthn': 'http://localhost:'+serverPort,
-		},
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './client/public/index.html',
-			favicon: './client/public/favicon.ico',
-		}),
-	],
+  entry: ["core-js/stable", "regenerator-runtime/runtime", "./client/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // 這裡可以省略，因為已經有 .babelrc
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    proxy: [
+      {
+        context: ["/webauthn"],
+        target: `http://localhost:${serverPort}`,
+      },
+    ],
+    hot: true,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./client/public/index.html",
+      favicon: "./client/public/favicon.ico",
+    }),
+  ],
+  mode: "development",
+  devtool: "inline-source-map",
 };
